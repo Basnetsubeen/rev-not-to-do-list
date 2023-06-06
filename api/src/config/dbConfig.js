28c6aa47613f999d63f989be74bb0426f41e0a09
subin basnet <Basnetsubeen>
2023-06-07 09:25:09 +1000

    uodated name

--- api/src/config/dbConfig.js
+++ api/src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = () => {
   try {
-    const conn = mongoose.connect(process.env.Clouddb);
+    const conn = mongoose.connect(process.env.MONDO_DB);
     conn && console.log("Mongodb Connected");
   } catch (error) {
     console.log(error);
